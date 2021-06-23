34d32a951064b4193272accc88ccbf5f993761bd
Maksym Herasymov <herasymov.maks@gmail.com>
2021-06-23 22:28:06 +0300

    feature: fastify-objection plugin

---
+++ errors.js
@@ -0,0 +1,4 @@
+module.exports = {
+  ERR_MISSING_REQUIRED: '"connection" and "models" are required',
+  ERR_INVALID_PROPERTY: '"models" must be a valid array',
+};
---
+++ plugin.js
@@ -0,0 +1,39 @@
+const fp = require('fastify-plugin');
+const knex = require('knex');
+const objection = require('objection');
+
+const { ERR_MISSING_REQUIRED, ERR_INVALID_PROPERTY } = require('./errors');
+
+module.exports = fp((instance, options, next) => {
+  if (!options.connection || !options.models) {
+    next(new Error(ERR_MISSING_REQUIRED));
+  }
+
+  if (!Array.isArray(options.models) || !options.models.length) {
+    next(new Error(ERR_INVALID_PROPERTY));
+  }
+
+  const connection = knex({
+    client: 'pg',
+    connection: options.connection,
+    ...objection.knexSnakeCaseMappers(),
+  });
+
+  const models = {};
+
+  objection.Model.knex(connection);
+
+  options.models.forEach((model) => {
+    models[model.name] = model;
+  });
+
+  instance.decorate('models', models);
+  instance.decorate('knex', connection);
+
+  instance.addHook('onClose', (_, done) => {
+    connection.destroy();
+    done();
+  });
+
+  next();
+});
