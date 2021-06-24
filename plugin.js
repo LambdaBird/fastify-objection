66fd01028428ef230f41957a96384d5d2516e7ca
Maksym Herasymov <herasymov.maks@gmail.com>
2021-06-24 09:45:11 +0300

    feature: add metadata

--- plugin.js
+++ plugin.js
@@ -4,36 +4,42 @@ const objection = require('objection');
 
 const { ERR_MISSING_REQUIRED, ERR_INVALID_PROPERTY } = require('./errors');
 
-module.exports = fp((instance, options, next) => {
-  if (!options.connection || !options.models) {
-    next(new Error(ERR_MISSING_REQUIRED));
-  }
+module.exports = fp(
+  (instance, options, next) => {
+    if (!options.connection || !options.models) {
+      next(new Error(ERR_MISSING_REQUIRED));
+    }
 
-  if (!Array.isArray(options.models) || !options.models.length) {
-    next(new Error(ERR_INVALID_PROPERTY));
-  }
+    if (!Array.isArray(options.models) || !options.models.length) {
+      next(new Error(ERR_INVALID_PROPERTY));
+    }
 
-  const connection = knex({
-    client: 'pg',
-    connection: options.connection,
-    ...objection.knexSnakeCaseMappers(),
-  });
+    const connection = knex({
+      client: 'pg',
+      connection: options.connection,
+      ...objection.knexSnakeCaseMappers(),
+    });
 
-  const models = {};
+    const models = {};
 
-  objection.Model.knex(connection);
+    objection.Model.knex(connection);
 
-  options.models.forEach((model) => {
-    models[model.name] = model;
-  });
+    options.models.forEach((model) => {
+      models[model.name] = model;
+    });
 
-  instance.decorate('models', models);
-  instance.decorate('knex', connection);
+    instance.decorate('models', models);
+    instance.decorate('knex', connection);
 
-  instance.addHook('onClose', (_, done) => {
-    connection.destroy();
-    done();
-  });
+    instance.addHook('onClose', (_, done) => {
+      connection.destroy();
+      done();
+    });
 
-  next();
-});
+    next();
+  },
+  {
+    fastify: '>=2.0.0',
+    name: 'fastify-objection',
+  },
+);
